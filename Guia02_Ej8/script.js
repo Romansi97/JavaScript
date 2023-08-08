e585d9a6ae4f0434f8de267275592d719f2a3d69
Roman Ricciardi <romanezequielr97@gmail.com>
2023-08-08 19:51:36 -0300

    Ejer 8

--- Guia02_Ej8/script.js
+++ Guia02_Ej8/script.js
@@ -1,23 +1,52 @@
-let array = [];
+var arrayNumeros = [];
+var num = 0, suma = 0;
 
 do {
-   
-   var num = parseInt(prompt("Ingrese un número distinto de 0:"))
-   array.push(num);
-   if (num == 0) {
-      break;
-   }
-} while (num != 0);
-      
-   
-    
+  num = parseInt(prompt("Ingrese un número: "));
+  if (num !== 0) {
+    arrayNumeros.push(num);
+    suma = num + suma;
+  } else {
+    break;
+  }
+} while (num !== 0);
 
-   for (let i = 0; i < array.length; i++) {
-      alert(array[i]);
-      
-   }
-   
+if (arrayNumeros.length === 0) {
 
+} else {
 
+  var max = Math.max(...arrayNumeros);
+  var min = Math.min(...arrayNumeros);
+  console.log("El número menor es: " + min);
+  console.log("El número mayor es: " + max);
+  console.log("El promedio es: " + suma / arrayNumeros.length);
+  console.log(arrayNumeros);
+}
 
+/*FALTA SACAR EL PROMEDIO!! */
 
+/*var arrayNumeros = [];
+var num;
+
+do {
+  num = parseInt(prompt("Ingrese un número: "));
+  if (num !== 0) {
+    arrayNumeros.push(num);
+  } else {
+    break;
+  }
+} while (num !== 0);
+
+if (arrayNumeros.length === 0) {
+
+} else {
+
+  var max = Math.max(...arrayNumeros);
+  var min = Math.min(...arrayNumeros);
+  console.log("El número menor es: " + min);
+  console.log("El número mayor es: " + max);
+
+  console.log(arrayNumeros);
+}
+
+/*FALTA SACAR EL PROMEDIO!! */
