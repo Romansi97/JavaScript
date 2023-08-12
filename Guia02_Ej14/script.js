bd659b6b485543563875bc050417b1f32e5df1cf
Roman Ricciardi <romanezequielr97@gmail.com>
2023-08-11 21:34:44 -0300

    Ej14 listo

--- Guia02_Ej14/script.js
+++ Guia02_Ej14/script.js
@@ -1,3 +1,4 @@
+/* CREO VARIOS OBJETOS: */
 function Libros(isbn, titulo, autor, npaginas) {
     this.isbn = isbn;
     this.titulo = titulo;
@@ -5,6 +6,29 @@ function Libros(isbn, titulo, autor, npaginas) {
     this.npaginas = npaginas;
 }
 
+function IngresarDatos() {
+    this.isbn = prompt("Ingrese el ISBN del libro:");
+    this.titulo = prompt("Ingrese el titulo del libro:");
+    this.autor = prompt("Ingrese el autor del libro:");
+    this.npaginas = prompt("Ingrese la cantidad de páginas del libro:");
+}
+
+IngresarDatos();
+var libro1 = new Libros(this.isbn, this.titulo, this.autor, this.npaginas);
+IngresarDatos();
+var libro2 = new Libros(this.isbn, this.titulo, this.autor, this.npaginas);
+
+console.log(libro1);
+console.log(libro2);
+
+/* CREANDO SIN METODO:
+
+function Libros(isbn, titulo, autor, npaginas) {
+    this.isbn = isbn;
+    this.titulo = titulo;
+    this.autor = autor;
+    this.npaginas = npaginas;
+}
     var isbn = prompt("Ingrese el ISBN del libro:");
     var titulo = prompt("Ingrese el titulo del libro:");
     var autor = prompt("Ingrese el autor del libro:");
@@ -12,9 +36,4 @@ function Libros(isbn, titulo, autor, npaginas) {
     
     var libro1 = new Libros(isbn, titulo, autor, npaginas);
 
-    console.log(libro1);
-
-/*14. Crear un objeto libro que contenga las siguientes propiedades: ISBN, Título, Autor, 
-Número de páginas. Crear un método para cargar un libro pidiendo los datos al usuario 
-y luego informar mediante otro método el número de ISBN, el título, el autor del libro y el 
-numero de páginas*/
+    console.log(libro1);*/
