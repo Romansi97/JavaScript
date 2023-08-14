8105ba0b38cf48037b7d618d0cf5491874166f2d
Roman Ricciardi <romanezequielr97@gmail.com>
2023-08-14 17:22:09 -0300

    Ej15

---
+++ Guia02_Ej15/script.js
@@ -0,0 +1,13 @@
+function CalcularArea(radio) {
+    let area = Math.PI * Math.pow(radio, 2);
+    return area;
+}
+
+function CalcularPerimetro(radio) {
+    let perimetro = 2 * Math.PI * radio;
+    return perimetro;
+}
+    const radio = prompt("Ingrese el radio del circulo");
+    console.log("El área es: "+CalcularArea(radio));
+    console.log("El perímetro es: "+CalcularPerimetro(radio));
+    
